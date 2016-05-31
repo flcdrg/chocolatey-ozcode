e2af5a5b1aaccb7c21fab38ab52c4dc51bb779d0
David Gardiner <david@gardiner.net.au>
2016-05-31 11:00:30 +0930

    Adding

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'ozcode'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.oz-code.com/files/OzCode_2.0.0.1029.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+
+  softwareName  = 'OzCode'
+  checksum      = 'AB0468A0483AF7C254216F1FC0D1A9CC'
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
